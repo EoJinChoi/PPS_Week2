2794ec31c71714b8a04c174a0065ec3be8b36d45
EoJinChoi <01031763910a@gmail.com>
2023-01-13 22:21:23 +0900

    complete

---
+++ src/Solution/Main.java
@@ -0,0 +1,17 @@
+package Solution;
+
+import java.util.Scanner;
+
+public class Main { // 20230104
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N, M;
+		
+		N = s.nextInt();
+		M = s.nextInt();
+		
+		System.out.println(N * M - 1);
+	}
+
+}
